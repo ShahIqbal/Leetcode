4df1101d3528abe60244d09c0fe662ba7dbfdd91
Shah Tausif Iqbal <shah.iqbal@mail.mcgill.ca>
2021-05-24 12:16:17 -0400

    Time: 131 ms (55.46%), Space: 115.4 MB (27.71%) - LeetHub

---
+++ time-based-key-value-store/time-based-key-value-store.java
@@ -0,0 +1,34 @@
+class TimeMap {
+
+        HashMap<String, TreeMap<Integer, String>> map;
+    /** Initialize your data structure here. */
+    public TimeMap() {
+
+        map = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        if(!map.containsKey(key)) {
+            map.put(key, new TreeMap<>());
+        }
+        map.get(key).put(timestamp, value);
+    }
+
+    public String get(String key, int timestamp) {
+        TreeMap<Integer, String> treeMap = map.get(key);
+        if(treeMap == null) {
+            return "";
+        }
+        
+        Integer floorKey = treeMap.floorKey(timestamp);
+        if(floorKey == null) return "";
+        else return treeMap.get(floorKey);
+    }
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap obj = new TimeMap();
+ * obj.set(key,value,timestamp);
+ * String param_2 = obj.get(key,timestamp);
+ */
