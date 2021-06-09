6ca6175594cad49498e0915349a2eefd28335355
Shah Tausif Iqbal <shah.iqbal@mail.mcgill.ca>
2021-06-08 23:18:49 -0400

    Time: 2 ms (51.60%), Space: 39.1 MB (8.98%) - LeetHub

---
+++ find-the-difference/find-the-difference.java
@@ -0,0 +1,17 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int charS = 0;
+        int charT = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+            charS += s.charAt(i);
+        }
+        
+        for(int i = 0; i < t.length(); i++) {
+            charT += t.charAt(i);
+        }
+        
+        return (char)(charT - charS);
+        
+    }
+}
